e393863759ad93f0238ca81d33bb2a7a3d4e2280
Lluis <lluissalaslozano@gmail.com>
2018-02-14 21:06:31 +0100

    d

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,22 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { CitiesComponent } from './cities/cities.component';
+import { CityListComponent } from './cities/city-list/city-list.component';
+import { CityDetailComponent } from './cities/city-detail/city-detail.component';
+import { CityItemComponent } from './cities/city-list/city-item/city-item.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CitiesComponent,
+    CityListComponent,
+    CityDetailComponent,
+    CityItemComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
