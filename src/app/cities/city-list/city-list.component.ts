8476ed439c92534e73dcca3bef9ca9173ba070ed
Lluis <lluissalaslozano@gmail.com>
2018-02-14 21:58:23 +0100

    d

--- src/app/cities/city-list/city-list.component.ts
+++ src/app/cities/city-list/city-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { City } from '../city.model';
+
 @Component({
   selector: 'app-city-list',
   templateUrl: './city-list.component.html',
@@ -7,7 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CityListComponent implements OnInit {
 
-  cities = [];
+  cities: City[] = [
+    new City("3124724", "Santiago", ""),
+    new City("3435910", "Buenos Aires", ""),
+    new City("3936456", "Lima", ""),
+    new City("3448439", "Sao Pablo", ""),
+  ];
 
   constructor() { }
 
--- src/app/cities/city.model.ts
+++ src/app/cities/city.model.ts
@@ -2,10 +2,12 @@ export class City {
     public id: string;
     public name: string;
     public imagePath: string;
+    public temp: number;
 
     constructor(id:string, name: string, imagePath: string) {
         this.id = id;
         this.name = name;
         this.imagePath = imagePath;
+        this.temp = 0;
     }
 }
